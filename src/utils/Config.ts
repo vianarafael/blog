2b1541c086c2449830d617237503aad5a132b2c3
Rafael Viana <vianarafaelds@gmail.com>
2021-09-01 21:36:40 +0900

    first post

---
+++ src/utils/Config.ts
@@ -0,0 +1,9 @@
+export const Config = {
+  site_name: 'Rafael Viana',
+  title: 'Rafa\'s Blog',
+  description: 'Journaling the coding experience',
+  url: 'https://rafaelviana.io',
+  locale: 'en',
+  author: 'Rafael Viana',
+  pagination_size: 5,
+};
