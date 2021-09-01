451a4b217d10e2f7a55eaa2cd04984abacf2d4d1
Rafael Viana <vianarafaelds@gmail.com>
2021-09-01 22:08:12 +0900

    fixed the about page and added a rocket favicon

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
 
+import Head from 'next/head';
+
 import { Content } from '../content/Content';
 import { Meta } from '../layout/Meta';
 import { Main } from '../templates/Main';
 
 const About = () => (
-  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+  <Main meta={<Meta title="Who" description="Who" />}>
+    <Head>
+      <link
+        href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css"
+        rel="stylesheet"
+      />
+      <link
+        href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css"
+        rel="stylesheet"
+      />
+    </Head>
     <Content>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga recusandae quidem.
-        Quaerat molestiae blanditiis doloremque possimus labore voluptatibus distinctio recusandae
-        autem esse explicabo molestias officia placeat, accusamus aut saepe.
+        I am a multidisciplinary software engineer and web developer.
+        <br />
+        I work together to design, create and produce work that I am proud of for folks that I
+        believe in.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga recusandae quidem.
-        Quaerat molestiae blanditiis doloremque possimus labore voluptatibus distinctio recusandae
-        autem esse explicabo molestias officia placeat, accusamus aut saepe.
+        Hit me up
+        <div>
+          <a href="https://twitter.com/vianarafaelds" target="_blank" rel="noreferrer">
+            <i className="icon-twitter" />
+          </a>
+          {' '}
+          <a href="https://github.com/vianarafael" target="_blank" rel="noreferrer">
+            <i className="icon-github" />
+          </a>
+          {' '}
+          <a href="https://www.linkedin.com/in/rafael-viana/" target="_blank" rel="noreferrer">
+            <i className="icon-linkedin" />
+          </a>
+        </div>
       </p>
     </Content>
   </Main>
